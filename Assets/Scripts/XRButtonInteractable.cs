54d156881ecec1a65b296819a2d7539c581dc136
Cedeno718 <cedeno718@morrisville.edu>
2025-04-02 13:53:37 -0400

    Initial commit

---
+++ Assets/Scripts/XRButtonInteractable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+public class XRButtonInteractable : XRSimpleInteractable
+{
+    [SerializeField] Image buttonImage;
+    
+   [SerializeField] private Color normalColor;
+   [SerializeField] private Color hightlightedColor;
+   [SerializeField] private Color pressedColor;
+   [SerializeField] private Color selectedColor;
+    private bool isPressed;
+
+    void Start()
+    {
+        ResetColor();
+    }
+protected override void OnHoverEntered(HoverEnterEventArgs args)
+{
+    base.OnHoverEntered(args);
+    isPressed = false;
+    buttonImage.color = hightlightedColor;
+}
+protected override void OnHoverExited(HoverExitEventArgs args)
+{
+    base.OnHoverExited(args);
+    if(!isPressed)
+    {
+        buttonImage.color = normalColor;
+    }
+    
+}
+
+protected override void OnSelectEntered(SelectEnterEventArgs args)
+{
+    base.OnSelectEntered(args);
+    isPressed = true;
+    buttonImage.color = pressedColor;
+}
+protected override void OnSelectExited(SelectExitEventArgs args)
+{
+    base.OnSelectExited(args);
+    buttonImage.color = selectedColor;
+}
+    public void ResetColor()
+    {
+        buttonImage.color = normalColor;
+    }
+}
